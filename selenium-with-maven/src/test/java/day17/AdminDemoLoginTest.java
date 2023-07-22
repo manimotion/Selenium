6063710e33c52d5c0b0769a476cc948c793d4883
Jose Orjuela <jose.orjuela@perficient.com>
2023-07-21 22:17:06 -0500

    Adding the exercises of Day17 First Selenium Webdriver Script and the assugnement

--- selenium-with-maven/src/test/java/day17/AdminDemoLoginTest.java
+++ selenium-with-maven/src/test/java/day17/AdminDemoLoginTest.java
@@ -1,2 +1,50 @@
-package day17;public class AdminDemoLoginTest {
+package day17;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class AdminDemoLoginTest {
+    /*1) Launch Browser
+    2. open url: https://admin-demo.nopcommerce.com/login?ReturnUrl=%2Fadmin%2F
+    3. Provide username - admin@yourstore.com
+    4. provide password - admin
+    5. Click on Login button
+    6. Verify the title of dashboard page
+     */
+
+    public static void main(String[] args) throws InterruptedException {
+        //1) Launch Browser
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        //2. open Url
+        driver.get("https://admin-demo.nopcommerce.com/login?ReturnUrl=%2Fadmin%2F");
+        Thread.sleep(5000);
+        //3. Provide Username - admin@yourstore.com
+        driver.findElement(By.id("Email")).clear();
+        driver.findElement(By.id("Email")).sendKeys("admin@yourstore.com");
+        //4. Provide Password - admin
+        driver.findElement(By.id("Password")).clear();
+        driver.findElement(By.id("Password")).sendKeys("admin");
+        //5. Click on Login button
+        driver.findElement(By.xpath("/html/body/div[6]/div/div/div/div/div[2]/div[1]/div/form/div[3]/button")).click();
+        //6. Verify the title of dashboard page
+        String act_label = null;
+        try {
+            act_label = driver.findElement(By.xpath("/html/body/div[3]/div[1]/div[1]/h1")).getText();
+        } catch (NoSuchElementException e){
+            act_label = "";
+        }
+
+        String exp_label = "Dashboard";
+
+        if(act_label.equals(exp_label)){
+            System.out.println("Test Passed");
+        }
+        else {
+            System.out.println("failed");
+        }
+        driver.quit();
+    }
 }
--- selenium-with-maven/src/test/java/day17/OrangeHRMloginTest.java
+++ selenium-with-maven/src/test/java/day17/OrangeHRMloginTest.java
@@ -1,2 +1,71 @@
-package day17;public class OrangeHRMloginTest {
+package day17;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+/*
+1. Launch browser
+2. open url "https://opensource-demo.orangehrmlive.com/"
+3. Provide Username - Admin
+4. Provide Password - admin123
+5. Click on Login button
+6. Verify the titlle of dashboard page
+    exp title: OrangeHRM
+7. close browser
+ */
+public class OrangeHRMloginTest {
+    public static void main(String[] args) throws InterruptedException {
+        //1. Launch browser
+        WebDriver driver = new ChromeDriver();
+        //2. open url "https://opensource-demo.orangehrmlive.com/"
+        driver.get("https://opensource-demo.orangehrmlive.com/");
+        driver.manage().window().maximize();
+        Thread.sleep(5000);
+        //3. Provide Username - Admin
+        WebElement txtUsername = driver.findElement(By.name("username"));
+        txtUsername.sendKeys("Admin");
+        //4. Provide Password - admin123
+        driver.findElement(By.name("password")).sendKeys("admin");
+        //5. Click on Login button
+        driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button")).click();
+        Thread.sleep(7000);
+        //6. Verify the titlle of dashboard page
+        //    exp title: OrangeHRM
+        // TITLE VALIDATION
+        /*
+        String act_title =driver.getTitle();
+        String exp_title = "OrangeHRM";
+
+        if(act_title.equals(exp_title)){
+            System.out.println("Test Passed");
+        }
+        else {
+            System.out.println("failed");
+        }
+        */
+
+        //label validation after valid login
+        String act_label = null;
+        try {
+            act_label = driver.findElement(By.xpath("//*[@id='app']/div[1]/div[1]/header/div[1]/div[1]/span/h6")).getText();
+        } catch (NoSuchElementException e){
+            act_label = "";
+        }
+
+        String exp_label = "Dashboard";
+
+        if(act_label.equals(exp_label)){
+            System.out.println("Test Passed");
+        }
+        else {
+            System.out.println("failed");
+        }
+
+        //7. close browser
+        //driver.close();
+        driver.quit();
+
+    }
 }
